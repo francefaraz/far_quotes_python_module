019f085b7486d69309d18bc8bc1f71583c3edfa2
Shaik Muneer <itsmuneerfar@gmail.com>
2025-01-10 12:28:42 +0530

    updated the code to fetch quotes

---
+++ test.py
@@ -0,0 +1,7 @@
+from quote_inspire_jokes.quote import get_quote
+from quote_inspire_jokes.inspire import get_inspiration
+from quote_inspire_jokes.joke import get_joke
+
+print(get_quote())
+print(get_inspiration())
+print(get_joke())
